398ab317afcb563396266e3ee72b8d5c9b705715
Tre <tre@tre.one>
2019-08-29 21:15:46 +0300

    Реализована загрузка файлов проекта

--- classes/project.py
+++ classes/project.py
@@ -18,7 +18,7 @@ class ProjectDataStore(JsonDataStore):
         self.default_project_filepath = os.path.join(constants.DEFAULT_PROJECT_PATH, '_default.project')
 
         # Путь по умолчанию
-        self.current_filepath = None
+        self.current_file_path = None
 
         # Отслеживание изменений в проекте
         self.has_unsaved_changes = False
@@ -42,8 +42,8 @@ class ProjectDataStore(JsonDataStore):
         self._data["version"] = {constants.APP_NAME: constants.VERSION}
 
         # Пробуем сохранить файл настроек проекта (выкидывает ошибку при сбое)
-        self.write_to_file(file_path, self._data, path_mode="relative", previous_path=self.current_filepath)
-        self.current_filepath = file_path
+        self.write_to_file(file_path, self._data, path_mode="relative", previous_path=self.current_file_path)
+        self.current_file_path = file_path
 
         # Добавляем в "Последние файлы"
         self.add_to_recent_files(file_path)
@@ -91,17 +91,59 @@ class ProjectDataStore(JsonDataStore):
         # Сохраняем список
         settings.setValue("recent_projects", recent_projects)
 
-    # TODO: реализовать def load(self, file_path, clear_thumbnails=True):
+    def load(self, file_path, clear_images=True):
+        """Загрузка проекта из файла"""
+        self.load_default_project_settings()
+
+        if file_path:
+            log.info("Загрузка проекта: {}".format(file_path))
+
+            # Данные проекта по умолчанию
+            default_project = self._data
+
+            try:
+                # Загружаем проект
+                project_data = self.read_from_file(file_path, path_mode="absolute")
+            except Exception as ex:
+                raise ex
+
+            # Объединить параметры по умолчанию и параметры проекта
+            self._data = self.merge_settings(default_project, project_data)
+
+            self.current_file_path = file_path
+            self.has_unsaved_changes = False
+
+            # Копируем все изображения проекта в рабочую папку изображений
+            loaded_project_folder = os.path.dirname(self.current_file_path)
+            project_images_folder = os.path.join(loaded_project_folder, "images")
+            if os.path.exists(project_images_folder) and clear_images:
+                # Удаляем удаляем каталог с изображениями
+                shutil.rmtree(constants.IMAGES_PATH, True)
+
+                # Копируеем каталог с изображениями из папки проекта в рабочий каталог
+                shutil.copytree(project_images_folder, constants.IMAGES_PATH)
+
+            # Добавляем путь в список "Последние файлы"
+            self.add_to_recent_files(file_path)
+
+            # Обновление всех структур данных
+            self.upgrade_project_data_structures()
 
     def load_default_project_settings(self):
         """Загружает файл настроек проекта по умолчанию (выкидывает ошибку при сбое)"""
         self._data = self.read_from_file(self.default_project_filepath)
-
-        self.current_filepath = None
+        self.current_file_path = None
         self.has_unsaved_changes = False
-
         # Генерируем ID проекта
         self._data["id"] = self.generate_id()
+
+    def upgrade_project_data_structures(self):
+        """Устранение проблем с файлами проекта (если таковые имеются)"""
+        app_version = self._data["version"][constants.APP_NAME]
+        log.info("Применяем исправления проекта версии %s" % app_version)
+        # Исправить идентификатор проекта по умолчанию (если найден)
+        if self._data.get("id") == "T0":
+            self._data["id"] = self.generate_id()
 
     @staticmethod
     def generate_id(digits=10):
